c5054c056b758ef69d652bf9e5d7ecd25b99857c
Lovac42 <lovac42@protonmail.com>
2018-11-26 20:14:55 -0500

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # HoochieMama
 AnkiAddon: Ramdomize Queue
+
+Title is in reference to Seinfeld, no relations to the current slang term.
+
+
+## About:
+This is the backported _fillRev method from the v2 scheduler with some added features from serenityNow and works on both v1 and v2 scheduler. It allows randomization of subdecks on the v1 scheduler without switching to the v2 scheduler.
+
+### Enhancements:
+V2 ramdomizes subdecks but uses max reviews from the parent deck. This creates an imbalance where users with large amount of over due low priority cards could potentially end up focusing on one subdeck. A typical example: say Leaves, Clouds, Knots, and Math with a max review of 5 each and a cap at 20 for the parent deck. If the user have a large amount of overdues, say 25 each, the V2 scheduler would grab 20 cards sorted by dues resulting in 13 Leaves, 4 Clouds, 2 Knots, and 1 Math.
+
+The enhancement added is to focus on today's dues first. So for the previous example, it would be 4 dues today from each subdeck follow by any extra over dues from yesterday or later. Of course there are better enhancements that could have been made, but not for the general public and will probably degrade the performance of Anki. I see this as a reasonable compromise.
+
+
+## Configs:
+To prevent conflicts with other similar addons, this must be activated in preferences.
+
+Note: If you have serenityNow installed, please update to the latest version or disable it. If you have any other addons that also monkey patches _fillRev, disable them as well.
+
---
+++ src/hoochieMama.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+# Copyright: (C) 2018 Lovac42
+# Support: https://github.com/lovac42/HoochieMama
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+# Version: 0.0.1
+
+# Title is in reference to Seinfeld, no relations to the current slang term.
+
+import random
+import anki.sched
+from aqt import mw
+from anki.utils import ids2str, intTime
+from anki.hooks import wrap
+from anki import version
+ANKI21 = version.startswith("2.1.")
+
+
+
+#From: anki.schedv2.py
+#Mod:  names for backwards compatibility
+#Mod2: serenityNow, prioritize today first before overdues.
+#      This will even out the numbers imposed by the parent deck limits.
+
+def fillRev(self, _old):
+    qc = self.col.conf
+    if not qc.get("hoochieMama", False):
+        return _old(self)
+    # print('using hoochieMama')
+
+
+    if self._revQueue:
+        return True
+    if not self.revCount:
+        return False
+
+    lim = min(self.queueLimit, currentRevLimit())
+
+    if lim:
+        self._revQueue = self.col.db.list("""
+select id from cards where
+did in %s and queue = 2 and due = ?
+order by due
+limit ?""" % (ids2str(self.col.decks.active())),
+                self.today, lim)
+
+
+        more=lim-len(self._revQueue)
+        if more:
+            self._revQueue.extend(self.col.db.list("""
+select id from cards where
+did in %s and queue = 2 and due < ?
+order by due
+limit ?""" % (ids2str(self.col.decks.active())),
+                self.today, more))
+
+
+        if self._revQueue:
+            if self.col.decks.get(self.col.decks.selected(), default=False)['dyn']:
+                # dynamic decks need due order preserved
+                self._revQueue.reverse()
+            else:
+                # fixme: as soon as a card is answered, this is no longer consistent
+                r = random.Random()
+                # r.seed(self.today)
+                r.seed(intTime(1000))
+                r.shuffle(self._revQueue)
+            return True
+
+    if self.revCount:
+        # if we didn't get a card but the count is non-zero,
+        # we need to check again for any cards that were
+        # removed from the queue but not buried
+        self._resetRev()
+        return self._fillRev()
+
+
+#From: anki.schedv2.py
+def currentRevLimit():
+    d = mw.col.decks.get(mw.col.decks.selected(), default=False)
+    return deckRevLimitSingle(d)
+
+#From: anki.schedv2.py
+def deckRevLimitSingle(d, parentLimit=None):
+    # invalid deck selected?
+    if not d: return 0
+    if d['dyn']: return mw.col.sched.dynReportLimit
+
+    c = mw.col.decks.confForDid(d['id'])
+    lim = max(0, c['rev']['perDay'] - d['revToday'][1])
+    if parentLimit is not None:
+        return min(parentLimit, lim)
+    elif '::' not in d['name']:
+        return lim
+    else:
+        for parent in mw.col.decks.parents(d['id']):
+            # pass in dummy parentLimit so we don't do parent lookup again
+            lim = min(lim, deckRevLimitSingle(parent, parentLimit=lim))
+        return lim
+
+
+anki.sched.Scheduler._fillRev = wrap(anki.sched.Scheduler._fillRev, fillRev, 'around')
+if ANKI21:
+    import anki.schedv2
+    anki.schedv2.Scheduler._fillRev = wrap(anki.schedv2.Scheduler._fillRev, fillRev, 'around')
+
+
+##################################################
+#
+#  GUI stuff, adds preference menu options
+#
+#################################################
+import aqt
+import aqt.preferences
+from aqt.qt import *
+
+
+if ANKI21:
+    from PyQt5 import QtCore, QtGui, QtWidgets
+else:
+    from PyQt4 import QtCore, QtGui as QtWidgets
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+def setupUi(self, Preferences):
+    r=self.gridLayout_4.rowCount()
+    self.hoochieMama = QtWidgets.QCheckBox(self.tab_1)
+    self.hoochieMama.setObjectName(_fromUtf8("hoochieMama"))
+    self.hoochieMama.setText(_('Hoochie Mama! Randomize Queue'))
+    self.hoochieMama.toggled.connect(lambda:toggle(self))
+    self.gridLayout_4.addWidget(self.hoochieMama, r, 0, 1, 3)
+
+def __init__(self, mw):
+    qc = self.mw.col.conf
+    cb=qc.get("hoochieMama", 0)
+    self.form.hoochieMama.setCheckState(cb)
+
+def accept(self):
+    qc = self.mw.col.conf
+    qc['hoochieMama']=self.form.hoochieMama.checkState()
+
+def toggle(self):
+    checked=not self.hoochieMama.checkState()==0
+    if checked:
+        try:
+            self.serenityNow.setCheckState(0)
+        except: pass
+
+aqt.forms.preferences.Ui_Preferences.setupUi = wrap(aqt.forms.preferences.Ui_Preferences.setupUi, setupUi, "after")
+aqt.preferences.Preferences.__init__ = wrap(aqt.preferences.Preferences.__init__, __init__, "after")
+aqt.preferences.Preferences.accept = wrap(aqt.preferences.Preferences.accept, accept, "before")
