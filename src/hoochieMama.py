d185f1a026433d20397de6e32856ea52e8f685ad
Lovac42 <lovac42@protonmail.com>
2018-12-01 04:46:19 -0500

    refactored

--- src/hoochieMama.py
+++ src/hoochieMama.py
@@ -2,7 +2,7 @@
 # Copyright: (C) 2018 Lovac42
 # Support: https://github.com/lovac42/HoochieMama
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-# Version: 0.0.2
+# Version: 0.0.3
 
 # Title is in reference to Seinfeld, no relations to the current slang term.
 
@@ -20,7 +20,7 @@ CUSTOM_SORT = None #disabled
 
 #Anki's default QUEUE_LIMIT of 50 does not work well for parent decks,
 #so we changed it. Cheap windows tablets may need to lower this number.
-QUEUE_LIMIT = 250 #Deal size
+QUEUE_LIMIT = 256 #Deal size
 
 # == End Config ==========================================
 ##########################################################
@@ -38,52 +38,58 @@ ANKI21 = version.startswith("2.1.")
 
 
 #From: anki.schedv2.py
-#Mod:  names for backwards compatibility
-#Mod2: serenityNow, prioritize today first before overdues.
-#      This will even out the numbers imposed by the parent deck limits.
-#Mod3: added options for sorting by ivl.
-
+#Mod:  Various, see logs
 def fillRev(self, _old):
+    if self._revQueue:
+        return True
+    if not self.revCount:
+        return False
+
+
+    # This seem like old comments left behind, and does not affect current versions.
+    # Remove these lines for testing
+    if self.col.decks.get(self.col.decks.selected(),False)['dyn']:
+        # dynamic decks need due order preserved
+        return _old(self)
+
+
     qc = self.col.conf
     if not qc.get("hoochieMama", False):
         return _old(self)
     # print('using hoochieMama')
 
 
-    if self._revQueue:
-        return True
-    if not self.revCount:
-        return False
-
-#In the world of blackjack, “penetration”, or “deck penetration”, is the amount of cards that the dealer cuts off, relative to the cards dealt out.
-    PENETRATION=QUEUE_LIMIT if CUSTOM_SORT else self.queueLimit
-    lim=min(PENETRATION, currentRevLimit())
+# In the world of blackjack, “penetration”, or “deck penetration”, 
+# is the amount of cards that the dealer cuts off,
+# relative to the cards dealt out.
+    lim=currentRevLimit()
     if lim:
-        DYN=self.col.decks.get(self.col.decks.selected(),False)['dyn']
-        sortBy=CUSTOM_SORT if not DYN and CUSTOM_SORT else ''
+        if CUSTOM_SORT:
+            penetration=min(QUEUE_LIMIT,lim)
+            sortBy=CUSTOM_SORT
+        else: #Default
+            penetration=min(self.queueLimit,lim)
+            sortBy=''
 
         self._revQueue = self.col.db.list("""
 select id from cards where
 did in %s and queue = 2 and due = ?
-%s
-limit ?""" % (ids2str(self.col.decks.active()), sortBy),
-                self.today, lim)
+%s limit ?""" % (ids2str(self.col.decks.active()), sortBy),
+                self.today, penetration)
 
 
-        more=lim-len(self._revQueue)
+        more=penetration-len(self._revQueue)
         if more:
-            self._revQueue.extend(self.col.db.list("""
+            arr=self.col.db.list("""
 select id from cards where
 did in %s and queue = 2 and due < ?
-%s
-limit ?""" % (ids2str(self.col.decks.active()), sortBy),
-                self.today, more))
+%s limit ?""" % (ids2str(self.col.decks.active()), sortBy),
+                self.today, more)
+            if arr: self._revQueue.extend(arr)
 
 
         if self._revQueue:
-            if DYN: # dynamic decks need due order preserved
-                self._revQueue.reverse()
-            elif sortBy=='':
+            if not CUSTOM_SORT:
                 # fixme: as soon as a card is answered, this is no longer consistent
                 r = random.Random()
                 # r.seed(self.today)
@@ -107,7 +113,7 @@ def currentRevLimit():
 def deckRevLimitSingle(d, parentLimit=None):
     # invalid deck selected?
     if not d: return 0
-    if d['dyn']: return mw.col.sched.dynReportLimit
+    if d['dyn']: return 99999
 
     c = mw.col.decks.confForDid(d['id'])
     lim = max(0, c['rev']['perDay'] - d['revToday'][1])
