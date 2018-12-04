c4e6940c8fbec208685db792b1d20305f5ba1c8c
Lovac42 <lovac42@protonmail.com>
2018-12-03 23:49:49 -0500

    sync issue found, unfixed

--- src/hoochieMama.py
+++ src/hoochieMama.py
@@ -2,7 +2,7 @@
 # Copyright: (C) 2018 Lovac42
 # Support: https://github.com/lovac42/HoochieMama
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-# Version: 0.0.5
+# Version: 0.0.6
 
 # Title is in reference to Seinfeld, no relations to the current slang term.
 
@@ -48,7 +48,8 @@ def fillRev(self, _old):
         return True
     if not self.revCount:
         return False
-
+    if not self.col: #For Sync
+        return False
 
     # This seem like old comments left behind, and does not affect current versions.
     # Remove these lines for testing
@@ -66,7 +67,7 @@ def fillRev(self, _old):
 # In the world of blackjack, “penetration”, or “deck penetration”, 
 # is the amount of cards that the dealer cuts off,
 # relative to the cards dealt out.
-    lim=currentRevLimit()
+    lim=currentRevLimit(self)
     if lim:
         if CUSTOM_SORT:
             penetration=min(QUEUE_LIMIT,lim)
@@ -108,38 +109,43 @@ did in %s and queue = 2 and due < ?
         return self._fillRev()
 
 
-#From: anki.schedv2.py
-def currentRevLimit():
-    d = mw.col.decks.get(mw.col.decks.selected(), default=False)
-    return deckRevLimitSingle(d)
 
 
 #From: anki.schedv2.py
-def deckRevLimitSingle(d, parentLimit=None):
+def currentRevLimit(self):
+    d = self.col.decks.get(self.col.decks.selected(), default=False)
+    return deckRevLimitSingle(self, d)
+
+
+#From: anki.schedv2.py
+def deckRevLimitSingle(self, d, parentLimit=None):
     if not d: return 0  # invalid deck selected?
     if d['dyn']: return 99999
 
-    c = mw.col.decks.confForDid(d['id'])
+    c = self.col.decks.confForDid(d['id'])
     lim = max(0, c['rev']['perDay'] - d['revToday'][1])
     if parentLimit is not None:
         return min(parentLimit, lim)
     elif '::' not in d['name']:
         return lim
     else:
-        for parent in mw.col.decks.parents(d['id']):
+        for parent in self.col.decks.parents(d['id']):
             # pass in dummy parentLimit so we don't do parent lookup again
-            lim = min(lim, deckRevLimitSingle(parent, parentLimit=lim))
+            lim = min(lim, deckRevLimitSingle(self, parent, parentLimit=lim))
         return lim
+
+
 
 
 #For reviewer count display (cosmetic)
 #From: anki.schedv2.py
 def resetRevCount(self, _old):
+    if not self.col: return #For Sync
     qc = self.col.conf
     if not qc.get("hoochieMama", False):
         return _old(self)
 
-    lim = currentRevLimit()
+    lim = currentRevLimit(self)
     self.revCount = self.col.db.scalar("""
 select count() from (select id from cards where
 did in %s and queue = 2 and due <= ? limit %d)""" % (
@@ -184,11 +190,13 @@ def setupUi(self, Preferences):
     self.gridLayout_4.addWidget(self.hoochieMama, r, 0, 1, 3)
 
 def __init__(self, mw):
+    if not self.mw.col: return #For Sync
     qc = self.mw.col.conf
     cb=qc.get("hoochieMama", 0)
     self.form.hoochieMama.setCheckState(cb)
 
 def accept(self):
+    if not self.mw.col: return #For Sync
     qc = self.mw.col.conf
     qc['hoochieMama']=self.form.hoochieMama.checkState()
 
