05c5daddf4888d373f03007cb98d9fcf3a92cda5
Lovac42 <lovac42@protonmail.com>
2019-01-06 06:24:34 -0500

    n, neg rev cnt didn't get fix after all.

--- src/hoochieMama.py
+++ src/hoochieMama.py
@@ -2,7 +2,7 @@
 # Copyright: (C) 2018 Lovac42
 # Support: https://github.com/lovac42/HoochieMama
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-# Version: 0.1.8
+# Version: 0.1.9
 
 # Title is in reference to Seinfeld, no relations to the current slang term.
 
@@ -110,24 +110,24 @@ def fillRev(self, _old):
 
 # In the world of blackjack, “penetration”, or “deck penetration”, 
 # is the amount of cards that the dealer cuts off, relative to the cards dealt out.
-def getRevQueue(self, sortBy, penetration):
+def getRevQueue(sched, sortBy, penetration):
     debugInfo('v2 queue builder')
-    deckList=ids2str(self.col.decks.active())
+    deckList=ids2str(sched.col.decks.active())
     revQueue=[]
 
     if PRIORITIZE_TODAY:
-        revQueue = self.col.db.list("""
+        revQueue = sched.col.db.list("""
 select id from cards where
 did in %s and queue = 2 and due = ?
 %s limit ?""" % (deckList, sortBy),
-                self.today, penetration)
+                sched.today, penetration)
 
     if not revQueue:
-        revQueue = self.col.db.list("""
+        revQueue = sched.col.db.list("""
 select id from cards where
 did in %s and queue = 2 and due <= ?
 %s limit ?""" % (deckList, sortBy),
-                self.today, penetration)
+                sched.today, penetration)
 
     return revQueue #Order needs tobe reversed for custom sorts
 
@@ -136,6 +136,7 @@ did in %s and queue = 2 and due <= ?
 def getRevQueuePerSubDeck(sched,sortBy,penetration):
     debugInfo('per subdeck queue builder')
     revQueue=[]
+    sched._revDids=sched.col.decks.active()
     LEN=len(sched._revDids)
     if LEN>10: #auto shuffles >=10, 50/5
         if LEN>DECK_LIST_SHUFFLE_LIMIT: #segments
@@ -181,66 +182,66 @@ def cutDecks(queue,cnt=0):
 
 
 #From: anki.schedv2.py
-def currentRevLimit(self):
-    d = self.col.decks.get(self.col.decks.selected(), default=False)
-    return deckRevLimitSingle(self, d)
+def currentRevLimit(sched):
+    d = sched.col.decks.get(sched.col.decks.selected(), default=False)
+    return deckRevLimitSingle(sched, d)
 
 
 #From: anki.schedv2.py
-def deckRevLimitSingle(self, d, parentLimit=None):
+def deckRevLimitSingle(sched, d, parentLimit=None):
     if not d: return 0  # invalid deck selected?
     if d['dyn']: return 99999
 
-    c = self.col.decks.confForDid(d['id'])
+    c = sched.col.decks.confForDid(d['id'])
     lim = max(0, c['rev']['perDay'] - d['revToday'][1])
     if parentLimit is not None:
         return min(parentLimit, lim)
     elif '::' not in d['name']:
         return lim
     else:
-        for parent in self.col.decks.parents(d['id']):
+        for parent in sched.col.decks.parents(d['id']):
             # pass in dummy parentLimit so we don't do parent lookup again
-            lim = min(lim, deckRevLimitSingle(self, parent, parentLimit=lim))
+            lim = min(lim, deckRevLimitSingle(sched, parent, parentLimit=lim))
         return lim
 
 
 
 
 #For reviewer count display (cosmetic)
-def resetRevCount(self, _old):
+def resetRevCount(sched, _old):
     if on_sync:
-        return _old(self)
+        return _old(sched)
 
-    qc = self.col.conf
+    qc = sched.col.conf
     if not qc.get("hoochieMama", False):
-        return _old(self)
+        return _old(sched)
 
     if IMPOSE_SUBDECK_LIMIT:
-        return _resetRevCountV1(self)
-    return _resetRevCountV2(self)
+        return _resetRevCountV1(sched)
+    return _resetRevCountV2(sched)
 
 
 #From: anki.schedv2.py
-def _resetRevCountV2(self):
-    lim = currentRevLimit(self)
-    self.revCount = self.col.db.scalar("""
+def _resetRevCountV2(sched):
+    lim = currentRevLimit(sched)
+    sched.revCount = sched.col.db.scalar("""
 select count() from (select id from cards where
 did in %s and queue = 2 and due <= ? limit %d)""" % (
-        ids2str(self.col.decks.active()), lim), self.today)
+        ids2str(sched.col.decks.active()), lim), sched.today)
 
 #From: anki.sched.py
-def _resetRevCountV1(self):
+def _resetRevCountV1(sched):
     def _deckRevLimitSingle(d):
         if not d: return 0
         if d['dyn']: return 1000
         c = mw.col.decks.confForDid(d['id'])
         return max(0, c['rev']['perDay'] - d['revToday'][1])
     def cntFn(did, lim):
-        return self.col.db.scalar("""
+        return sched.col.db.scalar("""
 select count() from (select id from cards where
 did = ? and queue = 2 and due <= ? limit %d)""" % lim,
-                                      did, self.today)
-    self.revCount = self._walkingCount(
+                                      did, sched.today)
+    sched.revCount = sched._walkingCount(
         _deckRevLimitSingle, cntFn)
 
 
